e1a812c679881a056e84663fca2a2fe50300719b
Jun Yan <jyan.stat@gmail.com>
2019-04-13 06:20:40 -0400

    final presentation order

---
+++ presentation.R
@@ -0,0 +1,9 @@
+## The order of the presentation in the last two weeks is randomly assigned
+## with a random seed determined by all participants.
+
+presenters <- c("Chiovaro", "Fu", "Lau", "Jiao", "Palmer", "Qi", "Xu")
+
+set.seed(7026533)  # each presenter contributed one digit
+
+sample(presenters)
+
